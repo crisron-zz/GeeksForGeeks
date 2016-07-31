50c0ba5526a34771c6339aff9823bc5d904c1542
Saurabh Singhal <singhal.saurabh.mnnit@gmail.com>
2016-07-31 15:14:02 +0530

    Convert a binary tree to its mirror tree

---
+++ src/ConvertToMirror.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <cassert>
+
+#include "TreeHelper.h"
+
+using namespace std;
+
+// Return true if mirrorRoot is a mirror of root
+bool isMirror( struct node* mirrorRoot, struct node* root ) {
+    if( !mirrorRoot && !root ) {
+        return true;
+    }
+    if( ( !mirrorRoot && root ) || ( mirrorRoot && !root ) ) {
+        return false;
+    }
+    if( mirrorRoot->data != root->data ) {
+        return false;
+    }
+    return ( isMirror( mirrorRoot->left, root->right ) &&
+             isMirror( mirrorRoot->right, root->left ) );
+}
+
+void convertToMirror( struct node* root, struct node*& mirrorRoot ) {
+    if( !root ) {
+        return;
+    }
+    mirrorRoot = newNode( root->data );
+    convertToMirror( root->left, mirrorRoot->right );
+    convertToMirror( root->right, mirrorRoot->left );
+}
+
+int main() {
+    struct node* root, *mirrorRoot = NULL;
+
+    root = NULL;
+    convertToMirror( root, mirrorRoot );
+    assert( isMirror( mirrorRoot, root ) );
+
+    mirrorRoot = NULL;
+    root = newNode( 1 );
+    convertToMirror( root, mirrorRoot );
+    assert( isMirror( mirrorRoot, root ) );
+
+    mirrorRoot = NULL;
+    root = newNode( 1 );
+    root->left = newNode( 2 );
+    convertToMirror( root, mirrorRoot );
+    assert( isMirror( mirrorRoot, root ) );
+
+    mirrorRoot = NULL;
+    root = newNode( 1 );
+    root->right = newNode( 2 );
+    convertToMirror( root, mirrorRoot );
+    assert( isMirror( mirrorRoot, root ) );
+
+    mirrorRoot = NULL;
+    root = newNode( 1 );
+    root->left = newNode( 2 );
+    root->right = newNode( 3 );
+    convertToMirror( root, mirrorRoot );
+    assert( isMirror( mirrorRoot, root ) );
+
+    mirrorRoot = NULL;
+    root = newNode( 1 );
+    root->left = newNode( 2 );
+    root->right = newNode( 3 );
+    root->right->left = newNode( 4 );
+    root->right->right = newNode( 5 );
+    convertToMirror( root, mirrorRoot );
+    assert( isMirror( mirrorRoot, root ) );
+
+    cout << "\033[1;32m==========ALL TESTS PASSED==========\033[0m" << endl;
+    return 0;
+}
