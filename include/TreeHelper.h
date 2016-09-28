58a8cff88928aa00ed4373e7cdfd29cbb7d985a4
Saurabh Singhal <singhal.saurabh.mnnit@gmail.com>
2016-09-28 08:42:59 +0530

    Move isMirror to the header

--- include/TreeHelper.h
+++ include/TreeHelper.h
@@ -5,8 +5,6 @@ struct node {
     struct node* right;
 };
 
-// TODO: Put the implementation in a shared library
-// Keep only the declaration here
 // Create a new tree node
 struct node* newNode( int data ) {
     struct node *root = new node;
@@ -16,8 +14,22 @@ struct node* newNode( int data ) {
     return root;
 }
 
-// TODO: Put the implementation in a shared library
 bool isLeaf( struct node* root ) {
     return ( !root->left && !root->right );
+}
+
+// Return true if mirrorRoot is a mirror of root
+bool isMirror( struct node* mirrorRoot, struct node* root ) {
+    if( !mirrorRoot && !root ) {
+        return true;
+    }
+    if( ( !mirrorRoot && root ) || ( mirrorRoot && !root ) ) {
+        return false;
+    }
+    if( mirrorRoot->data != root->data ) {
+        return false;
+    }
+    return ( isMirror( mirrorRoot->left, root->right ) &&
+             isMirror( mirrorRoot->right, root->left ) );
 }
 
